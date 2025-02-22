eec67ee46660101e2082318e7f50b843ecce8ee6
VladyslavPilkevych <vladyslavpilkevych2004@gmail.com>
2025-02-22 13:46:54 +0100

    FE: translations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t, i18n } = useTranslation('translation');
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>{t('welcome')}</h1>
+      <p>{t('description')}</p>
+
+      <div>
+        <button onClick={() => changeLanguage('en')}>English</button>
+        <button onClick={() => changeLanguage('sk')}>Slovak</button>
+        <button onClick={() => changeLanguage('ua')}>Ukrainian</button>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/utils/translations/i18next.d.ts
@@ -0,0 +1,13 @@
+import 'react-i18next';
+
+declare module 'react-i18next' {
+  interface CustomTypeOptions {
+    defaultNS: 'translation';
+    resources: {
+      translation: {
+        welcome: string;
+        description: string;
+      };
+    };
+  }
+}
